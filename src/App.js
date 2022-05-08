b6c1355601669c8be8dce7db61da680f564d8a9b
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-05-08 23:24:43 +0300

    Header section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from './pages/Home/home';
+import Header from './component/header/header';
 
 function App() {
   return (
     <div className="App">
-      <div>Header</div>
+      <Header />
       <Home />
       <div>Footer</div>
     </div>
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './header.css';
 
-const header = () => (
-  <div>header</div>
+const Header = () => (
+  <div className="header-wrap">
+    <div className="header">
+      <div className="header-logo">
+        <span className="e">E</span>
+        xpenser
+      </div>
+      <div className="header-btn">
+        <a className="github" href="https://github.com/sentayhu19/Expenser" target="_blank" rel="noopener noreferrer">
+          <i aria-label="GitHub " role="button" i className="devicon-github-original colored" />
+          Star
+        </a>
+      </div>
+    </div>
+  </div>
 );
-export default header;
+export default Header;
